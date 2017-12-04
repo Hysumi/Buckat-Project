cabf3b655e58114ab93f2166bef2d07b1bd761d6
Henrique Sumitomo <hysumi@gmail.com>
2017-12-03 22:23:27 -0200

    Comentando código

--- Assets/Scripts/InGame/Enemy/EnemyController.cs
+++ Assets/Scripts/InGame/Enemy/EnemyController.cs
@@ -16,11 +16,10 @@ public class EnemyController : MonoBehaviour
 
     public GameObject kitten;
     public List<GameObject> kittenList;
-    public float kittenHeight;
+    public float kittenHeight; //Altura do kitten em relação ao chão
 
-    public int wave = 1, actualWaveCap = 6;
-    public int maxEnemys = 20;
-    public int actualEnemysLenght;
+    public int wave = 1;
+    public int maxEnemys = 10;
 
     #endregion
 
@@ -33,9 +32,8 @@ public class EnemyController : MonoBehaviour
 
     #region Spawn
 
-
     public float minSpawnDistance, maxSpawnDistance;
-    public float smoothSpawn;
+    public float smoothSpawn; //Ajuste para que o Spawn haja de maneira mais uniforme
     bool isSpawn = true;
 
     #endregion
@@ -44,7 +42,6 @@ public class EnemyController : MonoBehaviour
     {
         pc = GetComponent<PlayerController>();
         pc.wave = wave;
-        actualEnemysLenght = Mathf.RoundToInt(actualWaveCap * Mathf.Log(actualWaveCap, maxEnemys));
     }
 
     void Update()
@@ -63,7 +60,7 @@ public class EnemyController : MonoBehaviour
             pc.wave = wave;
         }
         
-        if(isSpawn)//actualEnemysLenght > kittenList.Count && 
+        if(isSpawn)//maxenemy > kittenList.Count && 
         {
             SummonKitten();
             isSpawn = false;
@@ -77,15 +74,17 @@ public class EnemyController : MonoBehaviour
         kittenList.Add(k);
     }
 
-    Vector3 GetSpawnPosition()
+    Vector3 GetSpawnPosition() //Função para definir a posição em que o inimigo irá dar Spawn
     {
         Vector3 spawnPos;
         float x, z;
         switch (wave)
         {
-            case 1:
+            case 1: /*
+                    * Ângulo de visão: -45~45.
+                    */
                 x = Random.Range(-maxSpawnDistance, maxSpawnDistance);
-                if(Mathf.Abs(x) > Mathf.Sqrt(2) * minSpawnDistance / 2)
+                if(Mathf.Abs(x) > Mathf.Sqrt(2) * minSpawnDistance / 2) //Caso o valor de X esteja fora do ângulo de visão
                 {
                     z = Random.Range(Mathf.Abs(x), maxSpawnDistance);
                 }
@@ -93,11 +92,14 @@ public class EnemyController : MonoBehaviour
                 {
                     z = Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) - Mathf.Pow(x, 2)), maxSpawnDistance);
                 }
+
                 spawnPos = new Vector3(x, kittenHeight, z); 
                 return spawnPos;
-            case 2:
+            case 2:/*
+                    * Ângulo de visão: -90~90.
+                    */
                 x = Random.Range(-maxSpawnDistance, maxSpawnDistance) * smoothSpawn;
-                if(Mathf.Abs(x) > maxSpawnDistance)
+                if(Mathf.Abs(x) > maxSpawnDistance) //Ajuste para que tenha uma maior chance de Z tender a 0
                 {
                     x = Mathf.Sign(x) * (maxSpawnDistance - Random.Range(0, maxSpawnDistance-minSpawnDistance));
                     z = Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) - Mathf.Pow(x, 2)), maxSpawnDistance) / smoothSpawn;
@@ -110,14 +112,17 @@ public class EnemyController : MonoBehaviour
                 {
                     z = Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) - Mathf.Pow(x, 2)), maxSpawnDistance);
                 }
+
                 spawnPos = new Vector3(x, kittenHeight, z);
                 return spawnPos;
-            case 3:
+            case 3:/*
+                    * Ângulo de visão: -135~135.
+                    */
                 z = Random.Range(-maxSpawnDistance, maxSpawnDistance);
                 if (z >= 0)
                 {
                     z *= smoothSpawn;
-                    if (Mathf.Abs(z) > maxSpawnDistance)
+                    if (Mathf.Abs(z) > maxSpawnDistance) //Ajuste para que tenha uma maior chance de X tender a 0
                     {
                         z = Mathf.Sign(z) * (maxSpawnDistance - Random.Range(0, maxSpawnDistance - minSpawnDistance));
                         x = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) -
@@ -133,12 +138,13 @@ public class EnemyController : MonoBehaviour
                         x = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) -
                             Mathf.Pow(z, 2)), maxSpawnDistance);
                     }
+
                     spawnPos = new Vector3(x, kittenHeight, z);
                     return spawnPos;
                 }
                 else
                 {
-                    if (Mathf.Abs(z) > Mathf.Sqrt(2) * minSpawnDistance / 2)
+                    if (Mathf.Abs(z) > Mathf.Sqrt(2) * minSpawnDistance / 2) //Caso o valor de Z esteja dentro do ângulo de visão
                     {
                         x = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Abs(z), maxSpawnDistance);
                     }
@@ -149,9 +155,32 @@ public class EnemyController : MonoBehaviour
                                 Mathf.Pow(z, 2)), maxSpawnDistance);
                     }
                 }
+
                 spawnPos = new Vector3(x, kittenHeight, z);
                 return spawnPos;
-            case 4:
+            case 4:/*
+                    * Ângulo de visão: 360.
+                    */
+                x = Random.Range(-maxSpawnDistance, maxSpawnDistance) * smoothSpawn; 
+                if (Mathf.Abs(x) > maxSpawnDistance) //Ajuste para que tenha uma maior chance de Z tender a 0
+                {
+                    x = Mathf.Sign(x) * (maxSpawnDistance - Random.Range(0, maxSpawnDistance - minSpawnDistance));
+                    z = Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) - Mathf.Pow(x, 2)), maxSpawnDistance) / smoothSpawn;
+                }
+                else if (Mathf.Abs(x) > minSpawnDistance)
+                {
+                    z = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) - Mathf.Pow(x, 2)) - 1, maxSpawnDistance);
+                }
+                else
+                {
+                    z = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) - Mathf.Pow(x, 2)) - 1, maxSpawnDistance);
+                }
+
+                spawnPos = new Vector3(x, kittenHeight, z);
+                return spawnPos;
+            default:/*
+                    * Ângulo base de visão: 360.
+                    */
                 x = Random.Range(-maxSpawnDistance, maxSpawnDistance) * smoothSpawn;
                 if (Mathf.Abs(x) > maxSpawnDistance)
                 {
@@ -166,23 +195,7 @@ public class EnemyController : MonoBehaviour
                 {
                     z = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) - Mathf.Pow(x, 2)) - 1, maxSpawnDistance);
                 }
-                spawnPos = new Vector3(x, kittenHeight, z);
-                return spawnPos;
-            default:
-                x = Random.Range(-maxSpawnDistance, maxSpawnDistance) * smoothSpawn;
-                if (Mathf.Abs(x) > maxSpawnDistance)
-                {
-                    x = Mathf.Sign(x) * (maxSpawnDistance - Random.Range(0, maxSpawnDistance - minSpawnDistance));
-                    z = Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) - Mathf.Pow(x, 2)), maxSpawnDistance) / smoothSpawn;
-                }
-                else if (Mathf.Abs(x) > minSpawnDistance)
-                {
-                    z = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(maxSpawnDistance, 2) - Mathf.Pow(x, 2)) - 1, maxSpawnDistance);
-                }
-                else
-                {
-                    z = Mathf.Sign(Random.Range(-1, 1)) * Random.Range(Mathf.Sqrt(Mathf.Pow(minSpawnDistance, 2) - Mathf.Pow(x, 2)) - 1, maxSpawnDistance);
-                }
+
                 spawnPos = new Vector3(x, kittenHeight, z);
                 return spawnPos;
         }
